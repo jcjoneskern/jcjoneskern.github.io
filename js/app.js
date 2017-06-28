a6cd84100cd3e787a3b1a05905a3096dbe7b5377
J-C <jcjoneskern@gmail.com>
2017-06-28 11:39:25 -0400

    more changes

--- js/app.js
+++ js/app.js
@@ -9,12 +9,20 @@
       templateUrl: "views/about.html"
     })
     .when("/portfolio", {
-      templateUrl: "views/portfolio.html"
+      templateUrl: "views/portfolio.html",
+      controller: "portfolioCtrl"
     })
     .when("/contact", {
       templateUrl: "views/contact.html"
     })
     .otherwise("/");
+  });
+
+  app.directive("portfolioItem", function() {
+    return {
+      restrict: "EAC",
+      templateUrl: "views/partialportfolio.html"
+    }
   });
 
 })();
---
+++ js/portfolioctrl.js
@@ -0,0 +1,17 @@
+(function() {
+
+  var app = angular.module("portfolio");
+
+  app.controller('portfolioCtrl', function($scope) {
+    $scope.content = [
+      {
+        title: ,
+        liveurl: ,
+        ghurl: ,
+        img: ,
+        desc:
+      },
+    ];
+  });
+
+})();
