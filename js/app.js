87fb29f501246cdfcee264eec6801815afdee20f
J-C <jcjoneskern@gmail.com>
2018-01-18 19:10:24 -0500

    redoing layout again

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@
 
   app.config(function($routeProvider) {
     $routeProvider.when("/", {
-      templateUrl: "views/home.html"
+      templateUrl: "views/home.html",
+      controller: "portfolioCtrl"
     })
     .when("/about", {
       templateUrl: "views/about.html"
@@ -26,3 +27,22 @@
   });
 
 })();
+
+
+// window.scroll({
+//   top: 2500,
+//   left: 0,
+//   behavior: 'smooth'
+// });
+
+// // Scroll certain amounts from current position 
+// window.scrollBy({
+//   top: 100, // could be negative value
+//   left: 0,
+//   behavior: 'smooth'
+// });
+
+// // Scroll to a certain element
+// document.querySelector('.hello').scrollIntoView({
+//   behavior: 'smooth'
+// });
