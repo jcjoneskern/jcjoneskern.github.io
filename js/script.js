3c69f282f962d06e76d8da5293c05e88b86884af
J-C <jcjoneskern@gmail.com>
2018-01-18 23:03:19 -0500

    scroll down button, css transitions

--- js/script.js
+++ js/script.js
@@ -38,34 +38,6 @@ const portfolio = new Vue({
                 desc: "Uses Angular to fill in the blanks for a game of mad libs."
             },
             // {
-            //     viewUrl: "https://jcjoneskern.github.io/todo-list/",
-            //     img: "images/todolist.png",
-            //     srcUrl: "https://github.com/jcjoneskern/todo-list",
-            //     title: "To Do List",
-            //     desc: "Add new items to a to do list, delete completed items, and filter through the list to find a specific task."
-            // },
-            // {
-            //     viewUrl: "https://jcjoneskern.github.io/reddit-minus-reddit/",
-            //     img: "images/redditjson.png",
-            //     srcUrl: "https://github.com/jcjoneskern/reddit-minus-reddit",
-            //     title: "Reddit Minus Reddit",
-            //     desc: "Displays the front page of the chosen subreddit."
-            // },
-            // {
-            //     viewUrl: "https://jcjoneskern.github.io/rainy-night/",
-            //     img: "images/rainynight.png",
-            //     srcUrl: "https://github.com/jcjoneskern/rainy-night",
-            //     title: "Rainy Night",
-            //     desc: "An experiment with CSS animations."
-            // },
-            // {
-            //     viewUrl: "https://jcjoneskern.github.io/J-CLab8/",
-            //     img: "images/shoppinglist.png",
-            //     srcUrl: "https://github.com/jcjoneskern/J-CLab8",
-            //     title: "Shopping List",
-            //     desc: "Takes in user input to generate a shopping list and calculate the total cost."
-            // },
-            // {
             //     viewUrl: "https://github.com/jcjoneskern",
             //     img: "images/comingsoon.png",
             //     srcUrl: "https://github.com/jcjoneskern",
